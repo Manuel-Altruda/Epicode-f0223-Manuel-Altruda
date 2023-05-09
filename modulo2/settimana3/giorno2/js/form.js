0fe0374f8d1ade3992fb03b53821c3115427c674
Manuel Altruda <129069563+Manuel-Altruda@users.noreply.github.com>
2023-05-09 15:33:22 +0200

    giorno2 | form |

---
+++ modulo2/settimana3/giorno2/js/form.js
@@ -0,0 +1,28 @@
+// Recupera il valore precedentemente salvato dal LocalStorage, se presente
+const savedName = localStorage.getItem('name');
+if (savedName) {
+  document.getElementById('savedName').textContent = 'Il tuo nome salvato è: ' + savedName;
+}
+
+// Aggiungi l'evento al pulsante Salva
+const saveBtn = document.getElementById('saveBtn');
+    saveBtn.addEventListener('click', function() {
+    const name = document.getElementById('name').value;
+    localStorage.setItem('name', name);
+    document.getElementById('savedName').textContent = 'Il tuo nome salvato è: ' + name;
+});
+
+// Aggiungi l'evento al pulsante Rimuovi
+const clearBtn = document.getElementById('clearBtn');
+    clearBtn.addEventListener('click', function() {
+    localStorage.removeItem('name');
+    document.getElementById('savedName').textContent = '';
+});
+
+// Crea un contatore che tenga conto del tempo trascorso utilizzando sessionStorage
+let counter = sessionStorage.getItem('counter') || 0;
+    setInterval(function() {
+    counter++;
+    sessionStorage.setItem('counter', counter);
+    document.getElementById('counter').textContent = 'Tempo trascorso: ' + counter + ' secondi';
+}, 1000);
