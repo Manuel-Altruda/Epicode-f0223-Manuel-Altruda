aed907b829d110fd4f40ec4407e07b363e3aa963
Manuel Altruda <manuelaltruda8@gmail.com>
2023-05-22 15:03:02 +0200

    tutto

---
+++ modulo3/settimana1/giorno1/RandomGameNumber/js/script.js
@@ -0,0 +1,36 @@
+"use strict";
+function generaNumeroCasuale(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function calcolaDistanza(numero, target) {
+    return Math.abs(numero - target);
+}
+function confrontaGiocatori(giocatore1, giocatore2, numeroCasuale) {
+    const distanza1 = calcolaDistanza(giocatore1, numeroCasuale);
+    const distanza2 = calcolaDistanza(giocatore2, numeroCasuale);
+    if (giocatore1 === numeroCasuale && giocatore2 === numeroCasuale) {
+        return `Entrambi i giocatori hanno indovinato il numero casuale ${numeroCasuale}!`;
+    }
+    else if (giocatore1 === numeroCasuale) {
+        return `Il giocatore 1 ha indovinato il numero casuale ${numeroCasuale}!`;
+    }
+    else if (giocatore2 === numeroCasuale) {
+        return `Il giocatore 2 ha indovinato il numero casuale ${numeroCasuale}!`;
+    }
+    else {
+        if (distanza1 === distanza2) {
+            return `Nessuno dei due ha indovinato il numero casuale ${numeroCasuale}, ma entrambi si sono avvicinati alla stessa distanza.`;
+        }
+        else if (distanza1 < distanza2) {
+            return `Nessuno dei due ha indovinato il numero casuale ${numeroCasuale}, ma il giocatore 1 si è avvicinato di più.`;
+        }
+        else {
+            return `Nessuno dei due ha indovinato il numero casuale ${numeroCasuale}, ma il giocatore 2 si è avvicinato di più.`;
+        }
+    }
+}
+const giocatore1 = 5;
+const giocatore2 = 10;
+const numeroCasuale = generaNumeroCasuale(1, 100);
+const risultato = confrontaGiocatori(giocatore1, giocatore2, numeroCasuale);
+console.log(risultato);
