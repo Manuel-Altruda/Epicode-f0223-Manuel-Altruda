310c80e0702fd57d2648e3e836da60a0472f362c
Manuel Altruda <manuelaltruda8@gmail.com>
2023-05-24 15:06:11 +0200

    update scriptJS

--- modulo3/settimana1/giorno3/ts/script.js
+++ modulo3/settimana1/giorno3/ts/script.js
@@ -29,6 +29,40 @@ class lavoratoreAutonomoTipoB extends lavoratoreAutonomo {
         return this.redditoAnnuoLordo * 0.9;
     }
 }
+
+ // Funzione chiamata al clic del pulsante "Calcola"
+function calcolaTasse() {
+    const codredd = Number(document.getElementById('codredd').value);
+    const redditoAnnuoLordo = (document.getElementById('redditoAnnuoLordo').value);
+    const tasseInps = (document.getElementById('tasseInps').value);
+    const tasseIrpef = (document.getElementById('tasseIrpef').value);
+
+    let lavoratore;
+
+    if (codredd === 1) {
+      lavoratore = new lavoratoreAutonomoTipoA(codredd, redditoAnnuoLordo, tasseInps, tasseIrpef);
+    } else if (codredd === 2) {
+      lavoratore = new lavoratoreAutonomoTipoB(codredd, redditoAnnuoLordo, tasseInps, tasseIrpef);
+    } else {
+      alert("Codice di redditività non valido!");
+      return;
+    }
+
+    const tasseInpsOutput = lavoratore.getTasseInps();
+    const tasseIrpefOutput = lavoratore.getTasseIrpef();
+    const redditoAnnuoNetto = lavoratore.getRedditoAnnuoNetto();
+
+    document.getElementById('tasseInpsOutput').innerHTML = tasseInpsOutput.toString();
+    document.getElementById('tasseIrpefOutput').innerHTML = tasseIrpefOutput.toString();
+    document.getElementById('redditoAnnuoNetto').innerHTML = redditoAnnuoNetto.toString();
+    
+    // Svuotamento dei campi di input
+    document.getElementById('codredd').value = '';
+    document.getElementById('redditoAnnuoLordo').value = '';
+    document.getElementById('tasseInps').value = '';
+    document.getElementById('tasseIrpef').value = '';
+}
+
 // ora vado ad usare le classi
 const lavoratoreA = new lavoratoreAutonomoTipoA(1, 50000, 5000, 10000);
 console.log("Lavoratore A:");
@@ -41,4 +75,3 @@ console.log("Lavoratore B:");
 console.log("Tasse INPS:", lavoratoreB.getTasseInps());
 console.log("Tasse IRPEF:", lavoratoreB.getTasseIrpef());
 console.log("Reddito Annuo Netto:", lavoratoreB.getRedditoAnnuoNetto());
-//# sourceMappingURL=script.js.map
