478c80035826bbf55eed67f28895f7997a9963b8
Manuel Altruda <129069563+Manuel-Altruda@users.noreply.github.com>
2023-05-25 15:37:25 +0200

    TypeScript Ex.4

---
+++ modulo3/settimana1/giorno4/ts/script.js
@@ -0,0 +1,40 @@
+"use strict";
+class CapoAbbigliamento {
+    constructor(data) {
+        this.id = data.id;
+        this.codprod = data.codprod;
+        this.collezione = data.collezione;
+        this.capo = data.capo;
+        this.modello = data.modello;
+        this.quantita = data.quantita;
+        this.colore = data.colore;
+        this.prezzoIvaEsclusa = data.prezzoivaesclusa;
+        this.prezzoIvaInclusa = data.prezzoivainclusa;
+        this.disponibile = data.disponibile;
+        this.saldo = data.saldo;
+    }
+    mostraPrezzo() {
+        return `Prezzo: €${this.prezzoIvaInclusa}`;
+    }
+}
+// Fetch dei dati dal file JSON
+fetch('Abbigliamento.json')
+    .then(response => response.json())
+    .then(data => {
+    const capiAbbigliamento = [];
+    // Creazione delle istanze dei capi d'abbigliamento
+    for (const item of data) {
+        const capo = new CapoAbbigliamento(item);
+        capiAbbigliamento.push(capo);
+    }
+    console.log(capiAbbigliamento);
+    // Mostra il prezzo di alcuni capi d'abbigliamento
+    const capiDaMostrare = capiAbbigliamento.slice(0, 3); // Scegli i primi 3 capi
+    for (const capo of capiDaMostrare) {
+        console.log(capo.mostraPrezzo());
+    }
+})
+    .catch(error => {
+    console.error('Si è verificato un errore durante la fetch:', error);
+});
+//# sourceMappingURL=script.js.map
