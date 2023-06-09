46137553147c04a4846620c216ae656a53cc0da0
Manuel Altruda <manuelaltruda8@gmail.com>
2023-06-09 18:22:45 +0200

    ..

--- modulo3/settimana3/weekProject/app/src/app/app-routing.module.ts
+++ modulo3/settimana3/weekProject/app/src/app/app-routing.module.ts
@@ -9,8 +9,6 @@ const routes: Routes = [
 
   { path: '', redirectTo: 'pages/auth', pathMatch: 'full', },
 
-
-
   { path: 'pages/auth', loadChildren: () => import('./pages/auth/auth.module').then(m => m.AuthModule) },
 
   { path: 'pages/dashboard', loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule)
